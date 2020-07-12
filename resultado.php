4827fe92d7d5cc8d659e7922680eba77ac66632d
ComandPromt <vayase_a_ferrol@hotmail.com>
2020-07-13 00:59:54 +0200

    Add files via upload

--- resultado.php
+++ resultado.php
@@ -7,16 +7,55 @@ function deliver_response($status,$tipo,$valores){
 	header('Content-Type:application/json');
     header("HTTP/1.1 $status $status_message");
 	$response['respuesta'] = $status;
-	$response['tipos'] = $tipo;
-    $response['valores'] = $valores;
+	$response['tipos'] = (array)$tipo;
+    $response['valores'] = (array)$valores;
     $json_response = json_encode($response);
     echo $json_response;
 }
 
-if(isset($_GET['respuesta'])){
+function quitar_caracteres_raros($cadena){
 	
-	if($_GET['respuesta']){
+	$caracteres = '                 d2 d3 d4 d5 d6 1 _ 1 /       ±   %? % d0  &(    ½% #) # (\' ØÜ ØÞ = Æ ß æ ð ¢ £ ¤ ¥ ¦ § ¨ © ¬ ® ¯ ± ¶ · ¼ ½ ¾ × ÷ " \' < >   ☺ ☻ ♥ ♦ ♣ ♠ • ◘ ○ ◙ ♂ ♀ ♪ ♫ ☼ ► ◄ ↕ ‼ ▬ ↨ ↑ ↓ → ← ∟ ↔ ▲ ▼ ⌂ $ ░ ▒ ▓ │ ┤ Â Á À ╣ ║ ╗ ╝ ┐ └ ┴ ┬ ├ ─ ┼ ╚ ╔ ╩ ╦ ╠ ═ ╬ ┘ ┌ █ ▄ ▀ ■ ( )';
+   
+	$caracteres = explode(' ',$caracteres);
+   
+	$nchar      = count($caracteres);
+   
+	$base       = 0;
 
+	while($base<$nchar){
+	 
+		$cadena = str_replace($caracteres[$base],'',$cadena);
+
+		$base++;
+	}
+   
+	return $cadena;
+}
+
+function limpiar_array($datos){
+	
+	for($x=0;$x<count($datos);$x++){
+		
+		$datos[$x]=utf8_encode($datos[$x]);
+
+		$datos[$x]=quitar_caracteres_raros($datos[$x]);
+
+		$datos[$x]=str_replace('  ',' ',$datos[$x]);
+		
+		$datos[$x]=trim($datos[$x]);
+	}
+	
+	return $datos;
+}
+
+if(isset($_GET['respuesta'])){
+
+	if($_GET['respuesta']){
+	
+	$_SESSION['imagen']=limpiar_array($_SESSION['imagen']);
+
+		
 		if($_SESSION['tipo'][0]==null){
 			$_SESSION['tipo'][0]="null";
 		}
@@ -29,13 +68,15 @@ if(isset($_GET['respuesta'])){
 	}
 
 	else{
-		deliver_response(200, 'null','null');
+
+		deliver_response(200, $_SESSION['tipo'],$_SESSION['imagen']);
 	}
 	
 }
 
 else{
-	deliver_response(400, 'null','null');
+
+	deliver_response(400, $_SESSION['tipo'],$_SESSION['imagen']);
 }
 
 session_unset();
